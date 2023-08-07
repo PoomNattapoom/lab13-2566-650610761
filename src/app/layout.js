8403ff9ab46b0ebd8ea7e5b5fea1ce53f18c36fd
PoomNattapoom <rockyaggi@gmail.com>
2023-08-07 15:35:26 +0700

    completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Nattapoom Pothongsunun"
+            studentId="650610761"
           />
         </div>
       </body>
